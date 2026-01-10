557491e3f876b759e9f80316b2261dadf1af4248
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2026-01-10 14:58:33 +0200

    add style

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	hideSubcategoryItem();
 	teamSliderInit();
 	individualSliderInit();
+	openTabs();
 });
 const hideSubcategoryItem = () => {
   const steps = document.querySelectorAll('.sub__item');
@@ -530,5 +531,58 @@ const individualSliderInit = () => {
 
   steps.forEach((step, index) => {
     step.addEventListener('click', () => swiper.slideTo(index));
+  });
+};
+
+const openTabs = () => {
+  const tabGroups = document.querySelectorAll(".target__wrap"); 
+
+  tabGroups.forEach((group) => {
+    const tabsLinks = group.querySelectorAll(".target__list-item");
+    const allContentBlocks = group.querySelectorAll(".target__content");
+    let frontBlockId = tabsLinks[0].dataset.name; 
+
+    function addTabsActive() {
+      tabsLinks.forEach((button, index) => {
+        button.addEventListener("click", () => {
+          tabsLinks.forEach((otherButton) => {
+            otherButton.classList.remove("active");
+          });
+          button.classList.add("active");
+          showContent(button.dataset.name, index);
+        });
+      });
+    }
+
+    function updateActiveTab(index) {
+      tabsLinks.forEach((button, i) => {
+        if (i === index) {
+          button.classList.add("active");
+        } else {
+          button.classList.remove("active");
+        }
+      });
+    }
+
+    function changeSlide(blockId) {
+      allContentBlocks.forEach((block) => {
+        if (block.getAttribute("id") === blockId) {
+          block.style.display = "flex";
+          block.style.opacity = 1;
+        } else {
+          block.style.opacity = 0;
+          block.style.display = "none";
+        }
+      });
+      frontBlockId = blockId;
+    }
+
+    function showContent(itemName, index) {
+      changeSlide(itemName);
+      updateActiveTab(index);
+    }
+
+    addTabsActive();
+    showContent(frontBlockId, 0); 
   });
 };
